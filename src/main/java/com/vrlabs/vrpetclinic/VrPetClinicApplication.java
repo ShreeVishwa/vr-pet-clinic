e17c62d94293a4b33f6904eb4d70c74ce4e5b950
ShreeVishwa <reddyvishwatej176@gmail.com>
2020-07-24 10:05:57 -0400

    Initial Project Add

---
+++ src/main/java/com/vrlabs/vrpetclinic/VrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.vrlabs.vrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vrlabs/vrpetclinic/VrPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrlabs.vrpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
