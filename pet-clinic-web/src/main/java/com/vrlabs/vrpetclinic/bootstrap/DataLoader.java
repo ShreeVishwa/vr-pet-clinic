88608b7f5b072f1cd8358991bd69824667771b1c
ShreeVishwa <reddyvishwatej176@gmail.com>
2020-08-06 09:16:07 -0400

    Added pet type to startup bootstrap. Closes #27

--- pet-clinic-web/src/main/java/com/vrlabs/vrpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/vrlabs/vrpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.vrlabs.vrpetclinic.bootstrap;
 
 import com.vrlabs.vrpetclinic.model.Owner;
+import com.vrlabs.vrpetclinic.model.PetType;
 import com.vrlabs.vrpetclinic.model.Vet;
 import com.vrlabs.vrpetclinic.services.OwnerService;
+import com.vrlabs.vrpetclinic.services.PetTypeService;
 import com.vrlabs.vrpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michel");
