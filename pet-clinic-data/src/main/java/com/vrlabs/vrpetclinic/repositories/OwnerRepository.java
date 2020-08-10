73e7b0ec2b0bdc21bee90942955f317efc1c15a2
ShreeVishwa <reddyvishwatej176@gmail.com>
2020-08-10 09:01:24 -0400

    Implemented Owners Spring data jpa. Closes #35

--- pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.vrlabs.vrpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
