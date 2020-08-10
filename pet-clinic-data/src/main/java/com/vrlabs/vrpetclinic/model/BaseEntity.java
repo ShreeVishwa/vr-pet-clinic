fbe99293fbda93d445ab72093ae6d4c926af6e1f
ShreeVishwa <reddyvishwatej176@gmail.com>
2020-08-10 08:10:40 -0400

    Implement Base Entity. Closes #30

--- pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/model/BaseEntity.java
@@ -1,10 +1,19 @@
 package com.vrlabs.vrpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// This indicates that this class is a base class and will be inherited by other classes and no need to create a seperate
+// table for this class
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     // hibernate requires us to write them as box types(Long) instead of long because those types can be null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
