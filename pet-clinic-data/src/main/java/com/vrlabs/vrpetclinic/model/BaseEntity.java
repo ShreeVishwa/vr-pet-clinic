5e66a68715886f16170ccc762180f752afcaea3c
ShreeVishwa <reddyvishwatej176@gmail.com>
2020-08-11 10:16:06 -0400

    Added project lombok and made changes. Closes #46

--- pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/model/BaseEntity.java
@@ -1,5 +1,8 @@
 package com.vrlabs.vrpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -8,6 +11,8 @@ import java.io.Serializable;
 
 // This indicates that this class is a base class and will be inherited by other classes and no need to create a seperate
 // table for this class
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
@@ -15,12 +20,4 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/vrlabs/vrpetclinic/model/Owner.java
@@ -1,10 +1,17 @@
 package com.vrlabs.vrpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 // @Table indicates what the table name should be in the database
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -21,35 +28,4 @@ public class Owner extends Person {
     @Column(name = "telephone")
     private String telephone;
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
